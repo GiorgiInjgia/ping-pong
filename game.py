77b716a3cf5eac65f03442194106ef96b9013c2b
Giorgi <giorgi.injgia11@gmail.com>
2022-06-26 15:14:35 +0400

    second changes

--- game.py
+++ game.py
@@ -1,1 +1,3 @@
-print("here we will create the game")
+print("here we will create the game,")
+
+print("here goes the change")
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+ddad = 0
 
     # Use a breakpoint in the code line below to debug your script.
 print("hello world")  # Press Ctrl+F8 to toggle the breakpoint.
